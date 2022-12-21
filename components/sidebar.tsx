c3aecf83fa92f6aebfd266dc64d6b474487950d8
irakibul7 <irakibul568@gmail.com>
2022-12-22 01:27:57 +0600

    Add app layout

---
+++ components/sidebar.tsx
@@ -0,0 +1,38 @@
+import NextImage from 'next/image'
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+
+const Sidebar = () => {
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+        <Box paddingY="20px">
+            <Box width="120px" marginBottom="20px">
+                <NextImage src="/Spotify_Logo_RGB_Green.png" height={60} width={120}/>
+            </Box>
+        </Box>
+    </Box>
+  );
+};
+
+export default Sidebar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,42 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import "reset-css";
+import PlayerLayout from "../components/playerLayout";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#F5F5F5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":foucus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
+    </ChakraProvider>
+  );
+};
+export default App;
